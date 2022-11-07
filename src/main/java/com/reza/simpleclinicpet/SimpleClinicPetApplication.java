9de587ff79b148c6ffe3a65ecaf2050296f142b1
rezahtg <reza.hutagaol@students.amikom.ac.id>
2022-11-07 10:45:21 +0700

    first commit

---
+++ src/main/java/com/reza/simpleclinicpet/SimpleClinicPetApplication.java
@@ -0,0 +1,13 @@
+package com.reza.simpleclinicpet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleClinicPetApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimpleClinicPetApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/reza/simpleclinicpet/SimpleClinicPetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.reza.simpleclinicpet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleClinicPetApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
