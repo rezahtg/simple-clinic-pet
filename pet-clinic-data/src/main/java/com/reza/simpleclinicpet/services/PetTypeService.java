f5e60898e145be6df00f13acb2f35233132b58ac
rezahtg <reza.hutagaol@students.amikom.ac.id>
2022-11-11 11:28:43 +0700

    Adding Pet Type Map Service. Closes #27

---
+++ pet-clinic-data/src/main/java/com/reza/simpleclinicpet/services/PetTypeService.java
@@ -0,0 +1,7 @@
+package com.reza.simpleclinicpet.services;
+
+import com.reza.simpleclinicpet.model.PetType;
+
+public interface PetTypeService extends CrudService<PetType, Long> {
+
+}
---
+++ pet-clinic-data/src/main/java/com/reza/simpleclinicpet/services/map/PetTypeServiceMap.java
@@ -0,0 +1,36 @@
+package com.reza.simpleclinicpet.services.map;
+
+import com.reza.simpleclinicpet.model.PetType;
+import com.reza.simpleclinicpet.services.PetTypeService;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+
+@Service
+public class PetTypeServiceMap extends AbstractMapService<PetType, Long> implements PetTypeService {
+
+    @Override
+    public Set<PetType> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public PetType findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public PetType save(PetType object) {
+        return super.save(object.getId(), object);
+    }
+
+    @Override
+    public void delete(PetType object) {
+        super.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+}
