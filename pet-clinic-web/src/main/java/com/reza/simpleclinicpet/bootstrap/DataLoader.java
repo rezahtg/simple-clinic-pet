c67cbae3b0e3892d8838a5ed13da6dc81498f6fe
rezahtg <reza.hutagaol@students.amikom.ac.id>
2022-11-11 15:53:30 +0700

    Adding Pet Type On Bootstrap. Closes #31

--- pet-clinic-web/src/main/java/com/reza/simpleclinicpet/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/com/reza/simpleclinicpet/bootstrap/DataLoader.java
@@ -1,8 +1,10 @@
 package com.reza.simpleclinicpet.bootstrap;
 
 import com.reza.simpleclinicpet.model.Owner;
+import com.reza.simpleclinicpet.model.PetType;
 import com.reza.simpleclinicpet.model.Vet;
 import com.reza.simpleclinicpet.services.OwnerService;
+import com.reza.simpleclinicpet.services.PetTypeService;
 import com.reza.simpleclinicpet.services.VetService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
@@ -13,13 +15,25 @@ public class DataLoader implements CommandLineRunner {
     private final OwnerService ownerService;
     private final VetService vetService;
 
-    public DataLoader(OwnerService ownerService, VetService vetService) {
+    private final PetTypeService petTypeService;
+
+    public DataLoader(OwnerService ownerService, VetService vetService, PetTypeService petTypeService) {
         this.ownerService = ownerService;
         this.vetService = vetService;
+        this.petTypeService = petTypeService;
     }
 
     @Override
     public void run(String... args) throws Exception {
+
+        PetType dog = new PetType();
+        dog.setName("Dog");
+        PetType savedDogType = petTypeService.save(dog);
+
+        PetType cat = new PetType();
+        dog.setName("Cat");
+        PetType savedCatType = petTypeService.save(cat);
+
 
         Owner owner1 =  new Owner();
         owner1.setId(1L);
